b002ca47916483f6c6106f0cbb6dd72c74d4bf57
Judith <jbarrilnavarro@gmai.com>
2019-05-27 14:08:30 +0200

    Cambios restricción valores numericos en celdas formulario

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,8 +14,24 @@ function trampa ()
             //Retorno false
             event.preventDefault(); //Anula el evento producido por un usuario
             return false;
-            
+
             // location.href="http://www.yahoo.com"; // Si quieres reedirigir la página.
         }
-    
+
+}
+
+function Numeros(string)
+{
+    //Solo numeros
+	var out = '';
+    var filtro = '1234567890'; //Caracteres validos
+
+	//Recorrer el texto y verificar si el caracter se encuentra en la lista de validos
+    for (var i=0; i<string.length; i++)
+       if (filtro.indexOf(string.charAt(i)) != -1)
+		   //Se añaden a la salida los caracteres validos
+		   out += string.charAt(i);
+
+	//Retornar valor filtrado
+    return out;
 }
